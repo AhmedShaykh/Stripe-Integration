194bceba7a370a216a73652b67ade168710ede9f
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-26 21:36:36 +0500

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-4xl my-8 font-bold">
+                Stripe Integration In Next.JS
+            </h1>
+        </div>
+    )
+};
+
+export default Home;
