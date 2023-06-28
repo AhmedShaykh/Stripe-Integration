a73217c86024303818a0219deecf4e6a3699d7f5
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-28 22:11:06 +0500

    Update route.ts

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -15,7 +15,50 @@ export const POST = async (request: NextRequest) => {
 
     try {
 
-        const session = await stripe.checkout.sessions.create({});
+        if (body.length > 0) {
+
+            const session = await stripe.checkout.sessions.create({
+                submit_type: "pay",
+                mode: "payment",
+                payment_method_types: ["card"],
+                billing_address_collection: "auto",
+                shipping_options: [
+                    { shipping_rate: "shr_1NO1kuBYP40wltzrhGKJR1zY" },
+                    { shipping_rate: "shr_1NO1jCBYP40wltzrhRn0EU1H" },
+                ],
+                invoice_creation: {
+                    enabled: true,
+                },
+                line_items: body.map((item: any) => {
+                    return {
+                        price_data: {
+                            currency: "pkr",
+                            product_data: {
+                                name: item.name,
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        quantity: item.quantity,
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                            maximum: 10,
+                        },
+                    };
+                }),
+                phone_number_collection: {
+                    enabled: false,
+                },
+                success_url: `${request.headers.get("origin")}/success`,
+                cancel_url: `${request.headers.get("origin")}/?canceled=true`,
+            });
+
+        }
+        else {
+
+            return NextResponse.json({ message: "No Data Found" });
+
+        }
 
     } catch (error) {
 
